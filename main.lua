bf1a8bd299180bc5e55c86d9f8ee8630fb179f08
Odd-owl <superdavid3Dworld@gmail.com>
2025-05-11 11:21:22 +0200

    added glorp sharp and glorpstellation

--- main.lua
+++ main.lua
@@ -16,12 +16,14 @@ SMODS.Joker:take_ownership('glass', {atlas = 'Glorps', pos = {x = 1, y = 1}})
 SMODS.Joker:take_ownership('faceless', {atlas = 'Glorps', pos = {x = 2, y = 1}})
 SMODS.Joker:take_ownership('invisible', {atlas = 'Glorps', pos = {x = 3, y = 1}})
 SMODS.Joker:take_ownership('stencil', {atlas = 'Glorps', pos = {x = 4, y = 1}})
+SMODS.Joker:take_ownership('card_sharp', {atlas = 'Glorps', pos = {x = 5, y = 1}})
 
 SMODS.Joker:take_ownership('hanging_chad', {atlas = 'Glorps', pos = {x = 0, y = 2}})
 SMODS.Joker:take_ownership('photograph', {atlas = 'Glorps', pos = {x = 1, y = 2}})
 SMODS.Joker:take_ownership('baron', {atlas = 'Glorps', pos = {x = 2, y = 2}})
 SMODS.Joker:take_ownership('mime', {atlas = 'Glorps', pos = {x = 3, y = 2}})
 SMODS.Joker:take_ownership('burnt', {atlas = 'Glorps', pos = {x = 4, y = 2}})
+SMODS.Joker:take_ownership('constellation', {atlas = 'Glorps', pos = {x = 5, y = 2}})
 
 
 SMODS.Joker:take_ownership('caino', {atlas = 'Glorps', pos = {x = 0, y = 3}, soul_pos = {x = 0, y = 4}})
@@ -43,11 +45,13 @@ local function init()
     G.localization.descriptions.Joker["j_faceless"].name = {"Faceless glorp"}
     G.localization.descriptions.Joker["j_invisible"].name = {"Invisible glorp"}
     G.localization.descriptions.Joker["j_stencil"].name = {"Glorp stencil"}
-    G.localization.descriptions.Joker["j_hanging_chad"].name = {"Chad glorp"}
+    G.localization.descriptions.Joker["j_card_sharp"].name = {"Glorp sharp"}
+    G.localization.descriptions.Joker["j_hanging_chad"].name = {"Glorp chad"}
     G.localization.descriptions.Joker["j_photograph"].name = {"Photoglorp"}
     G.localization.descriptions.Joker["j_baron"].name = {"Glaron"}
     G.localization.descriptions.Joker["j_mime"].name = {"Glime"}
     G.localization.descriptions.Joker["j_burnt"].name = {"Burnt glorp"}
+    G.localization.descriptions.Joker["j_constellation"].name = {"Glorpstellation"}
     G.localization.descriptions.Joker["j_hologram"].name = {"Hologlorp"}
 end
 
