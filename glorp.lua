567c6bf2e58bd7f46b4335fa2e6a5b9c444d8d6a
Odd-owl <superdavid3Dworld@gmail.com>
2025-05-12 00:12:25 +0200

    Code rewrite to use Malverk instead of breaking when Malverk is enabled.

--- glorp.lua
+++ glorp.lua
@@ -1,72 +1,46 @@
-SMODS.Atlas{
-    key = 'Glorps',
-    path = 'glorps.png',
-    px = 71,
-    py = 95
+AltTexture {
+	key = "glorps",
+    set = "Joker",
+	path = "jokers.png",
+	original_sheet = true,
+	keys = {"j_joker", "j_stencil", "j_mime", "j_abstract", "j_constellation", "j_faceless", "j_green_joker", "j_card_sharp", "j_hologram", "j_baron", "j_photograph", "j_golden", "j_glass", "j_blueprint", "j_wee", "j_invisible", "j_brainstorm", "j_hanging_chad", "j_burnt", "j_caino", "j_triboulet", "j_yorick", "j_chicot", "j_perkeo"},    
+    loc_txt = {
+        name = "Glorplatro"
+    },
+    localization = {
+        j_joker = {name = "glorper"},
+        j_wee = {name = "Wee glorp"},
+        j_blueprint = {name = "Glorpprint"},
+        j_stencil = {name = "Glorp stencil"},
+        j_mime = {name = "Glime"},
+        j_abstract = {name = "Abstract glorp"},
+        j_constellation = {name = "Glorpstellation"},
+        j_brainstorm = {name = "Glorpstorm"},
+        j_green_joker = {name = "Extra green glorp"},
+        j_golden = {name = "Golden glorp"},
+        j_glass = {name = "Glass glorp"},
+        j_faceless = {name = "Faceless glorp"},
+        j_invisible = {name = "Invisible glorp"},
+        j_card_shard = {name = "Glorp sharp"},
+        j_hanging_chad = {name = "Glorp chad"},
+        j_photograph = {name = "Photoglorp"},
+        j_baron = {name = "Glaron"},
+        j_burnt = {name = "Burnt glorp"},
+        j_hologram = {name = "Hologlorp"},
+        j_caino = {name = "GlorpGun"},
+        j_triboulet = {name = "GlorpTrans"},
+        j_yorick = {name = "Ta"},
+        j_chicot = {name = "Nolorp"},
+        j_perkeo = {name = "Gaylorp"},
+    }
 }
-
-SMODS.Joker:take_ownership('joker', {atlas = 'Glorps', pos = {x = 0, y = 0}})
-SMODS.Joker:take_ownership('wee', {atlas = 'Glorps', pos = {x = 0, y = 0}})
-SMODS.Joker:take_ownership('blueprint', {atlas = 'Glorps', pos = {x = 1, y = 0}})
-SMODS.Joker:take_ownership('abstract', {atlas = 'Glorps', pos = {x = 2, y = 0}})
-SMODS.Joker:take_ownership('brainstorm', {atlas = 'Glorps', pos = {x = 3, y = 0}})
-SMODS.Joker:take_ownership('green_joker', {atlas = 'Glorps', pos = {x = 4, y = 0}})
-
-
-SMODS.Joker:take_ownership('golden', {atlas = 'Glorps', pos = {x = 0, y = 1}})
-SMODS.Joker:take_ownership('glass', {atlas = 'Glorps', pos = {x = 1, y = 1}})
-SMODS.Joker:take_ownership('faceless', {atlas = 'Glorps', pos = {x = 2, y = 1}})
-SMODS.Joker:take_ownership('invisible', {atlas = 'Glorps', pos = {x = 3, y = 1}})
-SMODS.Joker:take_ownership('stencil', {atlas = 'Glorps', pos = {x = 4, y = 1}})
-SMODS.Joker:take_ownership('card_sharp', {atlas = 'Glorps', pos = {x = 5, y = 1}})
-
-SMODS.Joker:take_ownership('hanging_chad', {atlas = 'Glorps', pos = {x = 0, y = 2}})
-SMODS.Joker:take_ownership('photograph', {atlas = 'Glorps', pos = {x = 1, y = 2}})
-SMODS.Joker:take_ownership('baron', {atlas = 'Glorps', pos = {x = 2, y = 2}})
-SMODS.Joker:take_ownership('mime', {atlas = 'Glorps', pos = {x = 3, y = 2}})
-SMODS.Joker:take_ownership('burnt', {atlas = 'Glorps', pos = {x = 4, y = 2}})
-SMODS.Joker:take_ownership('constellation', {atlas = 'Glorps', pos = {x = 5, y = 2}})
-
-
-SMODS.Joker:take_ownership('caino', {atlas = 'Glorps', pos = {x = 0, y = 3}, soul_pos = {x = 0, y = 4}})
-SMODS.Joker:take_ownership('triboulet', {atlas = 'Glorps', pos = {x = 1, y = 3}, soul_pos = {x = 1, y = 4}})
-SMODS.Joker:take_ownership('yorick', {atlas = 'Glorps', pos = {x = 2, y = 3}, soul_pos = {x = 2, y = 4}})
-SMODS.Joker:take_ownership('chicot', {atlas = 'Glorps', pos = {x = 3, y = 3}, soul_pos = {x = 3, y = 4}})
-SMODS.Joker:take_ownership('perkeo', {atlas = 'Glorps', pos = {x = 4, y = 3}, soul_pos = {x = 4, y = 4}})
-SMODS.Joker:take_ownership('hologram', {atlas = 'Glorps', pos = {x = 5, y = 3}, soul_pos = {x = 5, y = 4}})
-
-
-local function init()
-    G.localization.descriptions.Joker["j_joker"].name = {"Glorper"}
-    G.localization.descriptions.Joker["j_wee"].name = {"Wee glorp"}
-    G.localization.descriptions.Joker["j_blueprint"].name = {"Glorpprint"}
-    G.localization.descriptions.Joker["j_abstract"].name = {"Abstract glorp"}
-    G.localization.descriptions.Joker["j_brainstorm"].name = {"Glorpstorm"}
-    G.localization.descriptions.Joker["j_green_joker"].name = {"Extra green glorp"}
-    G.localization.descriptions.Joker["j_golden"].name = {"Golden glorp"}
-    G.localization.descriptions.Joker["j_glass"].name = {"Glass glorp"}
-    G.localization.descriptions.Joker["j_faceless"].name = {"Faceless glorp"}
-    G.localization.descriptions.Joker["j_invisible"].name = {"Invisible glorp"}
-    G.localization.descriptions.Joker["j_stencil"].name = {"Glorp stencil"}
-    G.localization.descriptions.Joker["j_card_sharp"].name = {"Glorp sharp"}
-    G.localization.descriptions.Joker["j_hanging_chad"].name = {"Glorp chad"}
-    G.localization.descriptions.Joker["j_photograph"].name = {"Photoglorp"}
-    G.localization.descriptions.Joker["j_baron"].name = {"Glaron"}
-    G.localization.descriptions.Joker["j_mime"].name = {"Glime"}
-    G.localization.descriptions.Joker["j_burnt"].name = {"Burnt glorp"}
-    G.localization.descriptions.Joker["j_constellation"].name = {"Glorpstellation"}
-    G.localization.descriptions.Joker["j_hologram"].name = {"Hologlorp"}
-    G.localization.descriptions.Joker["j_caino"].name = {"GlorpGun"}
-    G.localization.descriptions.Joker["j_triboulet"].name = {"GlorpTrans"}
-    G.localization.descriptions.Joker["j_yorick"].name = {"Ta"}
-    G.localization.descriptions.Joker["j_chicot"].name = {"Nolorp"}
-    G.localization.descriptions.Joker["j_perkeo"].name = {"Gaylorp"}
-
-
-end
-
-if SMODS.current_mod then
-    SMODS.current_mod.process_loc_text = init
-else
-    init()
-end
+TexturePack {
+    key = "Glorplatro",
+    textures = {
+        "glorp_glorps",
+    },
+    loc_txt = {
+        name = "Glorplatro",
+        text = {"Adds glorp to balatro"}
+    }
+}
